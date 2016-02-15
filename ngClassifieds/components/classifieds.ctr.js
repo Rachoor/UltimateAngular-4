44c54812293afe80e2d4bd803cee78dd58d26025
Randy Davis <randy_davis@outlook.com>
2016-02-15 02:47:22 -0600

    finish the toolbar and classified card

--- ngClassifieds/components/classifieds.ctr.js
+++ ngClassifieds/components/classifieds.ctr.js
@@ -9,9 +9,10 @@
 
     function ClassifiedsCtrl () {
         var vm = this;
-        vm.name = {
-            first: 'Randy',
-            last: 'Davis'
+        vm.classified = {
+            title: 'First Item',
+            price: '$1,000,000.00',
+            description: "A fancy used stove.  It is made of stainless steel and you can cook burgers on it.  It has a matching vent hood."
         }
 
         vm.message = "Hello World!!!!!!!!";
