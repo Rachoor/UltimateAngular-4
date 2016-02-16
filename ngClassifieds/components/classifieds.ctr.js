d41097222861874f590f5839d7660caa8c7a7d04
Randy Davis <randy_davis@outlook.com>
2016-02-15 23:04:56 -0600

    finish putting in the sidnav; kept getting error on md-autofocus on input field markup so i removed it

--- ngClassifieds/components/classifieds.ctr.js
+++ ngClassifieds/components/classifieds.ctr.js
@@ -5,15 +5,25 @@
         .module('ngClassifieds')
         .controller('ClassifiedsCtrl', ClassifiedsCtrl)
 
-    ClassifiedsCtrl.$inject = ['$http', 'classifiedsFactory'];
+    ClassifiedsCtrl.$inject = ['$http', 'classifiedsFactory', '$mdSidenav'];
 
-    function ClassifiedsCtrl ($http, classifiedsFactory) {
+    function ClassifiedsCtrl ($http, classifiedsFactory, $mdSidenav) {
         var vm = this;
+        vm.openSidebar = openSidebar;
+        vm.closeSidebar = closeSidebar;
 
         classifiedsFactory.getClassifieds()
             .then (function (classifieds) {
                 console.log(classifieds);
                 vm.classifieds = classifieds.data;
             })
+
+        function openSidebar (){
+            $mdSidenav('left').open();
+        }
+
+        function closeSidebar () {
+            $mdSidenav('left').close();
+        }
     }
 })();
