cad4ec380e96c858dee4ddf4f6d1416e3a1733be
Randy Davis <randy_davis@outlook.com>
2016-02-21 23:24:17 -0600

    finished how ui router works

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -6,14 +6,29 @@
         // Angular Modules
 
         // 3rd Party Modules
-        'ngMaterial'
+        'ngMaterial',
+        'ui.router'
         // Custom Modules
 
         ])
-        .config(function($mdThemingProvider){
+        .config(function($mdThemingProvider, $stateProvider){
             $mdThemingProvider.theme('default')
                 .primaryPalette('teal')
                 .accentPalette('orange');
+
+            $stateProvider
+                .state('one',  {
+                    url: '/stateone',
+                    template: '<h1>State One</h1>'
+                })
+                .state('two', {
+                    url: '/statetwo',
+                    template: '<h1>State Two</h1> <md-button ui-sref="two.more">Go To Nested State</md-button><ui-view></ui-view>'
+                })
+                .state('two.more', {
+                    url: '/more',
+                    template: '<h1>Nested State Two.More</h1>'
+                });
         })
 
         .directive("helloWorld", function () {
